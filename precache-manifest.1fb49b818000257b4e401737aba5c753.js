6ec72d709b35a316a235c69dd4e767309360255e
unknown <allu.s@lynk.co.in>
2020-07-23 20:46:56 +0530

    Updates

---
+++ precache-manifest.1fb49b818000257b4e401737aba5c753.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc94f747b9709cd1f1211966f250d105",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c603986cfedd13d04edd",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "9f2cdeb6468edf9ea7ad",
+    "url": "./static/js/2.d131c4a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d131c4a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c603986cfedd13d04edd",
+    "url": "./static/js/main.ca5dd40a.chunk.js"
+  },
+  {
+    "revision": "9f1372ae08f01defae64",
+    "url": "./static/js/runtime-main.61da9db5.js"
+  }
+]);
